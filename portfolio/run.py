3bc2f962af77180990cf0f44e0578761a2ee8d50
Maxwell-icharia <maxwellicharia@gmail.com>
2017-08-21 09:03:55 +0300

    [Create: Run file where server is initialised from]

---
+++ portfolio/run.py
@@ -0,0 +1,6 @@
+from portfolio.routes import app
+# importing file to be run from here
+
+if __name__ == '__main__':
+	app.run(debug = True, port = 5000)  # debug parameter to assist while finding
+# errors
