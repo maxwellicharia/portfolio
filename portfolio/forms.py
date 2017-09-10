1260fab6338905bbb031738283ae22e303273cef
Maxwell-icharia <maxwellicharia@gmail.com>
2017-09-10 21:32:14 +0300

    [Edit] - Add new field

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -7,16 +7,27 @@ class ContactForm(FlaskForm):
     """
     class with the blueprint in which the contact form will be derived from
     """
-    name = StringField('First & Last Name:', [validators.DataRequired()])
+    name = StringField('First & Last Name:', [validators.DataRequired(message =
+                                                                      "What is "
+                                                                      "your "
+                                                                      "First "
+                                                                      "and Last "
+                                                                      "name?"
+                                                                      "")])
     phone = IntegerField('Phone Number:', [validators.DataRequired(message =
                                                                    "Kindly "
                                                                    "leave your "
                                                                    "phone number"
                                                                    "")])
-    email = StringField('Email Address:', [validators.DataRequired(),
+    email = StringField('Email Address:', [validators.DataRequired(message =
+                                                                   "Leave your "
+                                                                   "email to "
+                                                                   "contact you"),
                                             validators.Email("your@email.com")])
-    subject = StringField('Subject:', [validators.DataRequired()])
-    message = TextAreaField('Your message:', [validators.DataRequired("Message "
-                                                                      "required")])
+    message = TextAreaField('Your message:', [validators.DataRequired("How can "
+                                                                      "we "
+                                                                      "assist "
+                                                                      "you?"
+                                                                      "")])
     proposal = FileField('Proposal')
     submit = SubmitField('Send Message')
