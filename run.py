cc2efb61e5c5913132dd36a430295acea545b8f2
Maxwell-icharia <maxwellicharia@gmail.com>
2017-10-05 13:23:47 +0300

    [Edit] - Config settings

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from routes import app
-# importing file to be run from here
+
 
 if __name__ == '__main__':
-	app.run(debug = True, port = 5000)  # debug parameter to assist while finding
-# errors
+	app.run()
